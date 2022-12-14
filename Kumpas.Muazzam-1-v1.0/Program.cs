88794c1bff2b54a30f723014de55954746712781
Semih Şen <halil.semih.sen@gmail.com>
2022-12-14 21:22:40 +0300

    Add project files.

---
+++ Kumpas.Muazzam-1-v1.0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Windows.Forms;
+
+namespace Kumpas.Muazzam_1_v1._0
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            var query = System.IO.File.ReadAllLines("data.txt")[0]; 
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl($"https://www.youtube.com/results?search_query={query}");
+            Thread.Sleep(500);
+            var randomVideo = new Random().Next(1, 10);
+            var video = driver.FindElement(By.XPath(
+                $"/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div[2]/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[{randomVideo}]"));
+            video.Click();
+            Thread.Sleep(500);
+            driver.SwitchTo().ActiveElement().SendKeys("m");
+            driver.SwitchTo().ActiveElement().SendKeys("f");
+           Application.Run();
+        }
+    }
+}
---
+++ Kumpas.Mübarek-1-v1.0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Media;
+using System.Windows.Forms;
+
+namespace Kumpas.Mübarek_1_v10
+{
+    static class Program
+    {
+        static void Main()
+        {
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = "fon.wav";
+            player.PlayLooping();
+            Application.Run();
+        }
+    }
+}
